785da91282030efdb0c4a58b89ef485d3116504d
PKNYO <147757429+PKNYO@users.noreply.github.com>
2024-02-10 11:24:49 +0100

    Finish the project
    I added th playGround function that permits to
    play a game until 3 points and tell you the final
    score when someone hit 3 points.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let computerSelection = getComputerChoice()
-let playerSelection = ""
+
+let playerSelection
+let playerScore = 0 
+let computerScore = 0
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3)
@@ -10,16 +12,16 @@ function getComputerChoice() {
 
 function getPlayerChoice(playerString) {
     if (playerString == "" || null || undefined) {
-        console.log('Please, write your choice.')
+        alert('Please, write your choice.')
         return
     } 
 
     let formatedString  = formateString(playerString)
 
     if (isValid(formatedString)) {
-        playerSelection = formatedString
+        return formatedString
     } else {
-        console.log('Please, write Rock, Paper or Scissors')
+        alert('Please, write Rock, Paper or Scissors')
     }
 }
 
@@ -37,7 +39,38 @@ function formateString(string) {
     return formatedString
 }
 
+function playRound(player, computer) {
+    if (player === 'Rock' && computer === 'Scissors' ||
+        player === 'Paper' && computer === 'Rock' ||
+        player === 'Scissors' && computer === 'Paper') {
+
+        playerScore++
+        return (`You won! ${player} beats ${computer}.`)
+    }  else if (player === computer) {
+        return (`That's a draw! ${player} vs ${computer}.`)
+    } else {
+        computerScore++
+        return (`You lose, ${computer} beats ${player}.`)
+    }
+}
+
+function playGame() {
+    console.log(playRound(getPlayerChoice(playerSelection), getComputerChoice())) // Cette retourne le resultat de la manche
+    
+    if (computerScore === 3) {
+        alert(`Computer wins ${playerScore} - ${computerScore}.`)
+        return
+    } else if (playerScore === 3) {
+        alert(`Player wins ${playerScore} - ${computerScore}.`)
+        return
+    }
+
+    playerSelection = prompt('Rock, Paper, Scissors?')
+
+    playGame()
+}
+
 playerSelection = prompt('Rock, Paper, Scissors?')
 
-getPlayerChoice(playerSelection)
+playGame()
 
